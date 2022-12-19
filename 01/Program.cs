73ba23626210a8c366008d75bef67aaf544a483e
Oleg <olegashepelev@gmail.com>
2022-12-20 01:01:28 +0600

    Finish four exersices

---
+++ 01/Program.cs
@@ -0,0 +1,10 @@
+﻿var randomNumber = new Random();
+int number = randomNumber.Next(10, 100);
+
+int a1 = number / 10;
+int a2 = number % 10;
+
+int max = a1;
+if (max < a2) max = a2;
+
+Console.Write($"{number} -> {max}");
---
+++ 02/Program.cs
@@ -0,0 +1,8 @@
+﻿var randomNumber = new Random();
+int number = randomNumber.Next(100, 1000);
+
+int a1 = number / 100;
+int a2 = number % 10;
+int res = a1 * 10 + a2;
+
+Console.Write($"{number} -> {res}");
